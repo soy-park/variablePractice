fdbcc853dc31c63880cd5cdba393ccb62fa9b094
Sara Park <sspark106@gmail.com>
2022-11-08 13:45:07 -0700

    Add string reassignments

--- variable.js
+++ variable.js
@@ -2,6 +2,14 @@ var name = "Sara";
 var favoriteFood = "noodles";
 var fruit = "apple";
 
+name = "Mia";
+favoriteFood = "chicken";
+fruit = "banana";
+
+console.log(name);
+console.log(favoriteFood);
+console.log(fruit);
+
 var age = 31
 var birthdate = 10061991
 
